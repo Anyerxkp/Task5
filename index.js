e06e4cfae6619b3701f2d3aa763e1dc054f07dfa
Anyer <90461487+Anyerxkp@users.noreply.github.com>
2023-04-19 16:53:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -47,6 +47,66 @@ app.get('/todos', async (req, res) => {
 });
 
 // The rest of your code remains unchanged
+// POST a new todo with validation
+app.post('/todos', [
+  check('title').notEmpty().withMessage('Title is required'),
+  check('description').notEmpty().withMessage('Description is required')
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { title, description } = req.body;
+  const newTodo = new Todo({ title, description, completed: false });
+
+  try {
+    await newTodo.save();
+    res.status(201).json(newTodo);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// PUT (update) a todo by ID with validation
+app.put('/todos/:id', [
+  check('title').notEmpty().withMessage('Title is required'),
+  check('description').notEmpty().withMessage('Description is required'),
+  check('completed').isBoolean().withMessage('Completed must be a boolean value')
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { id } = req.params;
+  const { title, description, completed } = req.body;
+
+  try {
+    const updatedTodo = await Todo.findByIdAndUpdate(id, { title, description, completed }, { new: true });
+    if (!updatedTodo) {
+      return res.status(404).json({ error: 'Todo not found' });
+    }
+    res.json(updatedTodo);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// DELETE a todo by ID
+app.delete('/todos/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedTodo = await Todo.findByIdAndDelete(id);
+    if (!deletedTodo) {
+      return res.status(404).json({ error: 'Todo not found' });
+    }
+    res.status(204).send();
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
